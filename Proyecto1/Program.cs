c7dc24c18dac06861fe14437a5251fc5b8fa3961
Alejandro <yesidmaya@gmail.com>
2019-06-03 15:43:17 -0500

    Practica Inicio C#

---
+++ Proyecto1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Proyecto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo...");
+            int a = 5;
+            int b = 3;
+            int resultado = 0;
+            resultado = a + b;
+
+            Console.WriteLine("LA SUMA ES: -------------->" + resultado.ToString());
+            Console.WriteLine("OPRIMA UNA TECLA PARA TERMINAR...");
+            Console.ReadLine();
+        }
+    }
+}
